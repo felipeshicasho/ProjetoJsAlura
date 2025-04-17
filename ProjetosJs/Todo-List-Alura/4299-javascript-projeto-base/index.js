c74fd8a4ef32a46158a36a5b30fbfa6f61ddef4b
Felipe Shicasho <109622843+felipeshicasho@users.noreply.github.com>
2025-04-17 11:21:48 -0300

    Add files via upload

---
+++ ProjetosJs/Todo-List-Alura/4299-javascript-projeto-base/index.js
@@ -0,0 +1,25 @@
+import { criarItemDaLista } from "./scripts/criarItemDaLista.js";
+
+const listaDeCompras = document.getElementById("lista-de-compras");
+const botaoAdicionar = document.getElementById("adicionar-item");
+
+
+botaoAdicionar.addEventListener("click", (evento) => {
+    evento.preventDefault();
+    const itemDaLista = criarItemDaLista();
+    listaDeCompras.appendChild(itemDaLista)
+    verificarListaVazia();
+})
+
+const mensagemListaVazia = document.querySelector(".mensagem-lista-vazia");
+
+function verificarListaVazia() {
+    const itensDaLista = listaDeCompras.querySelectorAll("li");
+    if (itensDaLista.length === 0) {
+        mensagemListaVazia.style.display = "block"
+    } else {
+        mensagemListaVazia.style.display = "none"
+    }
+}
+
+verificarListaVazia();
